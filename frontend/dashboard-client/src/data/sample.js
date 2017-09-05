da3ed6bf636e47f2e008857496a454cadb81ec0e
DM <djmedora@gmail.com>
2017-09-05 11:14:35 -0700

    added js sample data export

---
+++ frontend/dashboard-client/src/data/sample.js
@@ -0,0 +1,14 @@
+export default sample= [
+    {
+        "name": "Alice",
+        "age": 12
+    },
+    {
+        "name": "Michael",
+        "age": 13
+    },
+    {
+        "name": "Michael2",
+        "age": 14
+    }
+];
