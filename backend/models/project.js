b359b6a09e555332c250c2142e6c2f36c50a4718
Angelina Wang <angelina.wang@berkeley.edu>
2017-10-21 17:32:51 -0700

    changed schemas a bit

--- backend/models/project.js
+++ backend/models/project.js
@@ -3,6 +3,7 @@ var Schema = mongoose.Schema;
 
 var projectSchema = new mongoose.Schema({
 	_id: Schema.Types.ObjectId,
+	name: String,
     entities: [{ type: Schema.Types.ObjectId, ref: 'Entity' }],
     sources: [{ type: Schema.Types.ObjectId, ref: 'Source' }],
     users: [{ type: Schema.Types.ObjectId, ref: 'User' }]
