f075cbf24a7cf294c153364dc40e6ce7fdb42e58
DM <djmedora@gmail.com>
2017-10-28 22:43:33 -0700

    moved users functions to controller

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ var express = require('express'),
     LocalStrategy = require('passport-local'),
     User = require('./models/user'),
     MongoStore = require('connect-mongo')(session),
+    users_controller = require('./controllers/users'),
     configData = require('./config.js');
     
 app.use(bodyParser.urlencoded({extended: true}));
@@ -79,84 +80,18 @@ app.use(session(sessionOptions));
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-
 
 app.use(function(req, res, next) {
     res.locals.currentUser = req.user;
     next();
 });
 
-// app.post('/login', passport.authenticate('local', {
-//         successRedirect: frontend_url + '/links',
-//         failureRedirect: '/failed',
-//     }), function(req, res) {
-// });
-app.post('/api/login', function(req, res, next) {
-    passport.authenticate('local', function(err, user, info) {
-        if (err) {
-            return next(err);
-            // return res.send(err);
-        }
-        if (!user) {
-            // console.log("in !user: " + info.message);
-            // return res.send(info)
-            return res.json({ success: false, message: info.message });
-            // return res.redirect(frontend_url + '/loginpage');
-            // NOTE: redirects will cause CORS errors.
-        }
-
-        req.logIn(user, function(err) {
-            if (err) {
-                // return res.send(err);
-                return res.json({ success: false, message: err });
-                // return res.redirect(frontend_url + '/loginpage');
-            }
-            // return res.redirect(frontend_url + '/');
-            // res.send(user);
-            return res.json({ success: true, message: 'authentication succeeded' });
-        });
-    })(req, res, next)
-});
-
-
-app.get('/api/logout', function(req, res) {
-    req.logout();
-    return res.json({ success: true });
-    // res.send(res.json({success:true}));
-});
-
-
-app.post('/api/register', function(req, res) {
-    var u = {};
-    u.username = req.body.username;
-    var newUser = new User(u);
-    User.register(newUser, req.body.password, function(err, user) {
-        if (err) {
-            // res.send(err);
-            return res.json({ success: false, name: err.name, message: err.message });
-        } else {
-            passport.authenticate('local'
-                // ,{
-                // successRedirect: frontend_url + '/lol',
-                // failureRedirect: '/failed',
-                // }
-            ) (req, res, function () {
-                console.log("LOGGER: account created");
-                console.log(req._passport.session); // eg. { user: 'lol12' }
-                console.log(user); // just as user's entry in db appears
-                res.json({ success: true });
-                // res.send('Account created; Log in successful');
-                // res.redirect(frontend_url + '/links');
-            });
-        }
-   });
-});
-
-
+app.post('/api/login', users_controller.login);
+app.get('/api/logout', users_controller.logout);
+app.post('/api/register', users_controller.register);
 
 app.get('*', function(req, res) {
     res.status(404).send('Not found');
---
+++ backend/controllers/users.js
@@ -0,0 +1,71 @@
+var
+    // express = require('express'),
+    // session = require('express-session'),
+    // app = express(),
+    // mongoose = require('mongoose'),
+    // bodyParser = require('body-parser'),
+    passport = require('passport'),
+    // LocalStrategy = require('passport-local'),
+    User = require('../models/user');
+    // MongoStore = require('connect-mongo')(session),
+    // configData = require('./config.js');
+
+
+exports.login = function(req, res, next) {
+    passport.authenticate('local', function(err, user, info) {
+        if (err) {
+            return next(err);
+            // return res.send(err);
+        }
+        if (!user) {
+            // console.log("in !user: " + info.message);
+            // return res.send(info)
+            return res.json({ success: false, message: info.message });
+            // return res.redirect(frontend_url + '/loginpage');
+            // NOTE: redirects will cause CORS errors.
+        }
+
+        req.logIn(user, function(err) {
+            if (err) {
+                // return res.send(err);
+                return res.json({ success: false, message: err });
+                // return res.redirect(frontend_url + '/loginpage');
+            }
+            // return res.redirect(frontend_url + '/');
+            // res.send(user);
+            return res.json({ success: true, message: 'authentication succeeded' });
+        });
+    })(req, res, next)
+};
+
+exports.logout = function(req, res) {
+    req.logout();
+    return res.json({ success: true });
+    // res.send(res.json({success:true}));
+};
+
+exports.register = function(req, res) {
+    var u = {};
+    u.username = req.body.username;
+    var newUser = new User(u);
+    User.register(newUser, req.body.password, function(err, user) {
+        if (err) {
+            // res.send(err);
+            return res.json({ success: false, name: err.name, message: err.message });
+        } else {
+            passport.authenticate('local'
+                // ,{
+                // successRedirect: frontend_url + '/lol',
+                // failureRedirect: '/failed',
+                // }
+            ) (req, res, function () {
+                console.log("LOGGER: account created");
+                console.log(req._passport.session); // eg. { user: 'lol12' }
+                console.log(user); // just as user's entry in db appears
+                res.json({ success: true });
+                // res.send('Account created; Log in successful');
+                // res.redirect(frontend_url + '/links');
+            });
+        }
+    });
+};
