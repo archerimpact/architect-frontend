b987c151d5cba09efc5d1131551cb373e8750696
Alice Ma <ma.alice95@gmail.com>
2018-02-12 14:25:51 -0800

    redoing the canvas

---
+++ frontend/dashboard-client/src/pages/Canvas/index.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+import NodeGraph from '../../components/NodeGraph';
+import GraphContainer from './components/GraphContainer'
+
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as actions from '../../redux/actions/';
+import * as server from '../../server/';
+
+class Canvas extends Component {
+
+
+
+  render(){
+    return (
+
+    )
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(actions, dispatch),
+    dispatch: dispatch,
+  };
+}
+
+function mapStateToProps(state, props) {
+  if (state.data.savedEntities.status === 'isLoading' || state.data.savedSources.status === 'isLoading') {
+    return {
+      status: 'isLoading',
+      currentProject: state.data.currentProject
+    }
+  } else {
+    return {
+      status: 'isLoaded',
+      savedEntities: state.data.savedEntities,
+      projects: state.data.projects,
+      savedSources: state.data.savedSources,
+      currentProject: state.data.currentProject,
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Canvas);
