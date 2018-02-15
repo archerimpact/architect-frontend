224f9f72ff996a70bbfbf1e0c052d501bdaed375
Alice Ma <ma.alice95@gmail.com>
2018-02-15 15:34:32 -0800

    switching

--- frontend/dashboard-client/src/pages/BackendSearch/components/SearchEntityCard/index.js
+++ frontend/dashboard-client/src/pages/BackendSearch/components/SearchEntityCard/index.js
@@ -15,7 +15,7 @@ class SearchEntityCard extends Component {
           <p>{"neo4j_id: " + this.props.searchItem._source.neo4j_id} </p>
           <p>{"Name: " + this.props.searchItem._source.name}</p>
           <p>{"Nationality: " + this.props.searchItem._source.nationality} </p>
-        </div>
+        </div> 
       );
     } else {
       /* after the neo4j node item has loaded */
