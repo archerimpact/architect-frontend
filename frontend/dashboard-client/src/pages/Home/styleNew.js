7d466f4c230550465b6dfe347b8f4ba7f6e93c9c
Angelina Wang <angelina.wang@berkeley.edu>
2018-02-16 17:17:54 -0800

    delete unnecessary style

--- frontend/dashboard-client/src/pages/Home/styleNew.js
+++ frontend/dashboard-client/src/pages/Home/styleNew.js
@@ -11,18 +11,4 @@ export default {
             display: 'block'
         },
     },
-    searchBox: {
-        padding: '20px 20px 0 20px'
-    },
-    viewer: {
-        base: {
-            fontSize: '12px',
-            whiteSpace: 'pre-wrap',
-            backgroundColor: '#282C34',
-            border: 'solid 1px black',
-            padding: '20px',
-            color: '#9DA5AB',
-            minHeight: '250px'
-        }
-    }
 };
