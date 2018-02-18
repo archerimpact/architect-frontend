aa7b2975b4802bed3c8df2a36a6d1893bec82ce4
Michael Murphy <murphym1005@gmail.com>
2018-02-18 13:23:03 -0800

    Added SideBar files.

---
+++ frontend/dashboard-client/src/components/SideBar/index.js
@@ -0,0 +1,136 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as actions from '../../redux/actions/';
+
+import { Link } from 'react-router-dom';
+import * as server_utils from '../../server/utils';
+import { List, ListItem} from 'material-ui/List';
+import {red500, blue500} from 'material-ui/styles/colors';
+import Folder from 'material-ui/svg-icons/file/folder';
+
+import styles from './styleNew';
+import AddProject from './AddProject/';
+
+import {Treebeard, decorators} from 'react-treebeard';
+import {StyleRoot} from 'radium';
+import 'font-awesome/css/font-awesome.min.css';
+import './style.css';
+
+decorators.Header = ({style, node}) => {
+    const iconType = node.children ? 'folder-o' : 'file-text-o';
+    const iconClass = `fa fa-${iconType}`;
+    const iconStyle = {marginLeft: '5px', marginRight: '5px'};
+    const pathname = '/'+node.name + '/'+node.pid
+    return (
+         <div style={style.base}>
+             <div style={style.title}>
+                <i className={iconClass} style={iconStyle}/>
+                {node.children ? node.name : 
+                    <Link style={{color: 'inherit', textDecoration: 'none',}} to={pathname}> 
+                        {node.name}
+                    </Link>
+                }
+            </div>
+        </div>
+    );
+};
+
+class SideBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.onToggle = this.onToggle.bind(this);
+        this.projectList = this.projectList.bind(this);
+        this.addProject = this.addProject.bind(this);
+
+        const data =  this.projectList(this.props.projects);
+        this.state = {data};
+    }
+
+    componentDidMount() {
+        this.props.actions.fetchProjects();
+    }
+
+    componentWillReceiveProps(nextProps){
+        var newProjects = this.projectList(nextProps.projects);
+        this.setState({
+            data: newProjects
+        });
+    }
+
+    addProject(freshProject) {
+        server_utils.addProject(freshProject)
+        .then((data) => {
+            this.props.actions.fetchProjects();
+        })
+        .catch(error => {console.log(error)})
+      }
+
+    projectList(projects) {
+        const projectItems = projects.map((project) => {
+            return ({
+                name: project.name,
+                children: [
+                    { name: 'canvas', pid: project._id},
+                    { name: 'sources', pid: project._id},
+                    { name: 'entities', pid: project._id}
+                ]
+            })
+        });
+        return projectItems;
+    }
+
+    onToggle(node, toggled) {
+        const {cursor} = this.state;
+        if (cursor) {
+            cursor.active = false;
+        }
+        node.active = true;
+        if (node.children) {
+            node.toggled = toggled;
+        }
+
+        this.setState({cursor: node});
+    }
+
+    render() {
+        // Can reference current node with this.props.node
+        const {data: stateData, cursor} = this.state
+
+        return (
+            <div className="directory">
+                <AddProject submit={(freshProject)=>this.addProject(freshProject)} />
+                <StyleRoot>
+                    <div style={styles.component}>
+                        <Treebeard data={stateData}
+                                   decorators={decorators}
+                                   onToggle={this.onToggle}
+                                   style={styles}/>
+                    </div>
+                </StyleRoot>
+            </div>
+        );
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(actions, dispatch),
+        dispatch: dispatch,
+    };
+}
+
+function mapStateToProps(state) {
+    if (state.data.savedProjects.status === 'isLoading') {
+        return {
+            status: state.data.savedProjects.status,
+            }
+    } else {
+        return {
+            status: state.data.savedProjects.status,
+            projects: state.data.savedProjects.projects,
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SideBar);
