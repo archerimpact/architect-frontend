b4e32a63d8ed2bd072136d527c8e001067752856
Akshath Sivaprasad <akshath@berkeley.edu>
2018-03-26 18:49:26 -0700

    add sidebar with shortcuts

---
+++ public/js/sidebar.js
@@ -0,0 +1,88 @@
+function appendContentEntry(key) {
+  var leftText = document.createElement('div');
+  leftText.className = 'sidebar-left';
+  leftText.append(createDivElement('sidebar-text', key));
+
+  var rightText = document.createElement('div')
+  rightText.className = 'sidebar-right';
+  let text = false;
+  for (let arg of Array.prototype.slice.call(arguments, 1)) {
+    var element = text ? createDivElement('sidebar-text', arg) : createDivElement('sidebar-code', arg);
+    rightText.append(element);
+    text = !text;
+  }
+
+  var contentEntry = document.createElement('div');
+  contentEntry.className = 'content-entry';
+  contentEntry.append(leftText);
+  contentEntry.append(rightText);
+  $('#sidebar-content-area').append(contentEntry);
+}
+
+function createDivElement(className, text) {
+  var textElement = document.createElement('div');
+  textElement.className = className;
+  textElement.innerHTML = text;
+  return textElement;
+}
+
+function openSidebar() {
+  $("#sidebar-content-area").css("margin-right", '20px');
+  $("#sidebar").width(300);
+}
+
+function closeSidebar() {
+  $("#sidebar").width(0);
+  $("#sidebar-content-area").css("margin-right", 0);
+  $("#transparent-navbar i").removeClass("blue");
+}
+
+function quickSearch() {
+  $("#search-bar").select();
+}
+
+function navigate(link) {
+  window.location.href = `/${link}`;
+}
+
+$("#transparent-navbar i").click(function() {
+  $(this).toggleClass("blue");
+});
+
+$(document).ready(function() {
+  appendContentEntry('select nodes', 'click', '+', 'drag');
+  appendContentEntry('(un)select node', 'ctrl', '+', 'click');
+  appendContentEntry('(un)pin node', 'click');
+  appendContentEntry('unpin selected nodes', 'U');
+
+
+  let altDown = false;
+  $(document).keydown(function(e) {
+    // Listen for alt/command press
+    if (e.which == 18 || e.which == 91) {
+      altDown = true;
+    }
+
+    // Quick search with alt+s
+    if (altDown && e.which == 83) {
+      quickSearch();
+    }
+
+    // Open sidebar with alt+i
+    if (altDown && e.which == 73) {
+      openSidebar();
+    }
+
+    // Close sidebar with esc/enter
+    else if (e.which == 27 || e.which == 13) {
+      closeSidebar();
+    }
+  });
+
+  $(document).keyup(function(e) {
+    // Listen for alt/command press end
+    if (e.keyCode == 18 || e.keyCode == 91) {
+      altDown = false;
+    }
+  });
+});
