811105cf2399b36c1525d8e49ffd5217ff2286b2
Akshath Sivaprasad <akshath@berkeley.edu>
2018-03-29 01:12:26 -0700

    selection actions moved to right click!!!!!!

--- public/js/graph.js
+++ public/js/graph.js
@@ -8,23 +8,34 @@ const nodeSelection = {};
 const svg = d3.select('body')
       .append('svg')
       .attr('width', width)
-      .attr('height', height);
+      .attr('height', height)
+      .on("contextmenu", function (d, i) {
+        d3.event.preventDefault();
+      });
 
+// Setting up brush
 const brush = d3.svg.brush()
   .on('brushstart', brushstart)
   .on('brush', brushing)
   .on('brushend', brushend)
   .x(brushX).y(brushY);
 
-svg.append('g')
+const svgBrush = svg.append('g')
   .attr('class', 'brush')
   .call(brush);
 
+svg.on('mousedown', () => {
+  svgBrush.style('opacity', d3.event.button == 2 ? 1 : 0);
+});
+
+// Create force
 const force = d3.layout.force()
       .linkDistance(110)
       .size([width, height]);
 
-d3.json('34192.json', function(json) {
+
+// Load data and init graph
+d3.json('data.json', function(json) {
   force
       .gravity(1 / json.nodes.length)
       .charge(-1 * Math.max(Math.pow(json.nodes.length, 2), 750))
@@ -42,14 +53,6 @@ d3.json('34192.json', function(json) {
       .data(json.nodes)
       .enter().append('g')
       .attr('class', 'node')
-      .call(force.drag()
-        .on('dragstart', dragstart)
-        .on('drag', dragging)
-        .on('dragend', dragend)
-      );
-
-  node.append('circle')
-      .attr('r','15')
       .attr('dragfix', false)
       .attr('dragselect', false)
       .on('click', clicked)
@@ -58,6 +61,9 @@ d3.json('34192.json', function(json) {
         .on('drag', dragging)
         .on('dragend', dragend)
       );
+
+  node.append('circle')
+      .attr('r','15');
 
   node.append('text')
       .attr('dx', 22)
@@ -81,19 +87,21 @@ function brushstart() {
 }
 
 function brushing() {
-  const extent = brush.extent();
-  svg.selectAll('circle')
-    .classed('selected', function (d) {
-      const xPos = brushX.invert(d.x);
-      const yPos = brushY.invert(d.y);
-      const selected = (extent[0][0] <= xPos && xPos <= extent[1][0]
-                && extent[0][1] <= yPos && yPos <= extent[1][1])
-                || (this.classList.contains('selected') && d3.event.sourceEvent.ctrlKey);
-      nodeSelection[d.index] = selected;
-      return selected;
-    });
+  if (d3.event.sourceEvent.which == 3 || d3.event.sourceEvent.button == 2) {
+    const extent = brush.extent();
+    svg.selectAll('.node')
+      .classed('selected', function (d) {
+        const xPos = brushX.invert(d.x);
+        const yPos = brushY.invert(d.y);
+        const selected = (extent[0][0] <= xPos && xPos <= extent[1][0]
+                  && extent[0][1] <= yPos && yPos <= extent[1][1])
+                  || (this.classList.contains('selected') && d3.event.sourceEvent.ctrlKey);
+        nodeSelection[d.index] = selected;
+        return selected;
+      });
 
-  highlightLinksFromAllNodes();
+    highlightLinksFromAllNodes();
+  }
 }
 
 function brushend() {
@@ -105,43 +113,59 @@ function brushend() {
 function clicked(d, i) {
   if (d3.event.defaultPrevented) return;
   const node = d3.select(this);
-  const ctrlPressed = d3.event.ctrlKey.toString();
-  const fixed = node.attr('dragfix') == ctrlPressed;
-  const selected = node.attr('dragselect') != ctrlPressed;
-
-  node
-    .classed('fixed', d.fixed = fixed)
-    .classed('selected', selected);
-  nodeSelection[d.index] = selected;
-  highlightLinksFromNode(node[0]);
+  const fixed = !(node.attr('dragfix') == 'true');
+  if (d3.event.which == 1 || d3.event.button == 0) {
+    node.classed('fixed', d.fixed = fixed);
+  }
 
   force.resume();
   d3.event.stopPropagation();
 }
 
+function rightclicked(node, d) {
+  const fixed = node.attr('dragfix') == 'true';
+  const selected = !(node.attr('dragselect') == 'true');
+  node.classed('fixed', d.fixed = fixed)
+      .classed('selected', nodeSelection[d.index] = selected);
+  highlightLinksFromNode(node[0]);
+  force.resume();
+}
+
 // Click-drag node interactions
 function dragstart(d) {
   const node = d3.select(this);
-  const selected = d3.event.sourceEvent.ctrlKey;
   node
     .attr('dragfix', node.classed('fixed'))
     .attr('dragselect', node.classed('selected'))
-    .classed('active', true)
-    .classed('fixed', d.fixed = true)
-    .classed('selected', selected);
-  nodeSelection[d.index] = selected;
-  highlightLinksFromNode(node[0]);
+    .attr('dragdistance', 0)
+    // .classed('active', true)
+
+  node.classed('fixed', d.fixed = true);
+  if (d3.event.sourceEvent.which == 3 || d3.event.sourceEvent.button == 2) {
+    node.classed('selected', nodeSelection[d.index] = true);
+    highlightLinksFromNode(node[0]);
+  }
 } 
 
 function dragging(d) {
-  d3.select(this)
+  const node = d3.select(this);
+  node
     .attr('cx', d.x = d3.event.x)
-    .attr('cy', d.y = d3.event.y);
+    .attr('cy', d.y = d3.event.y)
+    .attr('dragdistance', parseInt(node.attr('dragdistance')) + 1);
 }
 
 function dragend(d) {
-  d3.selectAll('circle')
-    .classed('active', false);
+  
+  // d3.selectAll('circle')
+  //   .classed('active', false);
+  const node = d3.select(this);
+  console.log('dragend', node)
+  console.log('selected: '+node.classed('selected')+', dragselect: '+node.attr('dragselect'));
+  if (!parseInt(node.attr('dragdistance')) && (d3.event.sourceEvent.which == 3 || d3.event.sourceEvent.button == 2)) {
+    rightclicked(node, d);
+  }
+
   force.resume();
 }
 
@@ -150,12 +174,12 @@ d3.select('body')
   .on('keydown', function() {
     // u: Unpin selected nodes
     if (d3.event.keyCode == 85) {
-      svg.selectAll('circle.selected')
+      svg.selectAll('.node.selected')
         .each(function(d) { d.fixed = false; })
         .classed('fixed', false);
       force.resume();
     }
-  })
+  });
 
 // Link highlighting
 function highlightLinksFromAllNodes() {
