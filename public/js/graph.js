5bf58d1c4d8fc1cf01b833f9164d90e61f72fce9
Angelina Wang <angelina.wang@berkeley.edu>
2018-04-13 17:09:31 -0700

    must drag to zoom for some reason

--- public/js/graph.js
+++ public/js/graph.js
@@ -160,79 +160,81 @@ d3.json('data/34192.json', function(json) {
 
 });
 
-// function zoomed() {
-//   g.attr("transform", "translate(" + zoom.translate() + ")scale(" + zoom.scale() + ")");
-// }
+function zoomed() {
+  svgBrush.attr("transform", "translate(" + zoom.translate() + ")scale(" + zoom.scale() + ")");
+}
 
-// d3.select(self.frameElement).style("height", height + "px");
+d3.select(self.frameElement).style("height", height + "px");
 
-// // Simplest possible buttons
-// svg.selectAll(".button")
-//     .data(['zoom_in', 'zoom_out'])
-//     .enter()
-//     .append("rect")
-//     .attr("x", function(d,i){return 10 + 50*i})
-//     .attr({y: 10, width: 40, height: 20, class: "button"})
-//     .attr("id", function(d){return d})
-//     .style("fill", function(d,i){ return i ? "red" : "green"})
+// Simplest possible buttons
+svg.selectAll(".button")
+    .data(['zoom_in', 'zoom_out'])
+    .enter()
+    .append("rect")
+    .attr("x", function(d,i){return 10 + 50*i})
+    .attr({y: 10, width: 40, height: 20, class: "button"})
+    .attr("id", function(d){return d})
+    .style("fill", function(d,i){ return i ? "red" : "green"})
 
-// // Control logic to zoom when buttons are pressed, keep zooming while they are
-// // pressed, stop zooming when released or moved off of, not snap-pan when
-// // moving off buttons, and restore pan on mouseup.
+// Control logic to zoom when buttons are pressed, keep zooming while they are
+// pressed, stop zooming when released or moved off of, not snap-pan when
+// moving off buttons, and restore pan on mouseup.
 
-// var pressed = false;
-// d3.selectAll('.button').on('mousedown', function(){
-//     pressed = true;
-//     disableZoom();
-//     zoomButton(this.id === 'zoom_in')
-// }).on('mouseup', function(){
-//     pressed = false;
-// }).on('mouseout', function(){
-//     pressed = false;
-// })
-// svg.on("mouseup", function(){svg.call(zoom)});
+var pressed = false;
+d3.selectAll('.button').on('mousedown', function(){
+    pressed = true;
+    disableZoom();
+    zoomButton(this.id === 'zoom_in')
+}).on('mouseup', function(){
+    pressed = false;
+}).on('mouseout', function(){
+    pressed = false;
+})
+svg.on("mouseup", function(){svg.call(zoom)});
 
-// function disableZoom(){
-//     svg.on("mousedown.zoom", null)
-//        .on("touchstart.zoom", null)
-//        .on("touchmove.zoom", null)
-//        .on("touchend.zoom", null);
-// }
+function disableZoom(){
+    svg.on("mousedown.zoom", null)
+       .on("touchstart.zoom", null)
+       .on("touchmove.zoom", null)
+       .on("touchend.zoom", null);
+}
 
-// function zoomButton(zoom_in){
-//     var scale = zoom.scale(),
-//         extent = zoom.scaleExtent(),
-//         translate = zoom.translate(),
-//         x = translate[0], y = translate[1],
-//         factor = zoom_in ? 1.3 : 1/1.3,
-//         target_scale = scale * factor;
+function zoomButton(zoom_in){
+    var scale = zoom.scale(),
+        extent = zoom.scaleExtent(),
+        translate = zoom.translate(),
+        x = translate[0], y = translate[1],
+        factor = zoom_in ? 1.3 : 1/1.3,
+        target_scale = scale * factor;
 
-//     // If we're already at an extent, done
-//     if (target_scale === extent[0] || target_scale === extent[1]) { return false; }
-//     // If the factor is too much, scale it down to reach the extent exactly
-//     var clamped_target_scale = Math.max(extent[0], Math.min(extent[1], target_scale));
-//     if (clamped_target_scale != target_scale){
-//         target_scale = clamped_target_scale;
-//         factor = target_scale / scale;
-//     }
+    // If we're already at an extent, done
+    if (target_scale === extent[0] || target_scale === extent[1]) { return false; }
+    // If the factor is too much, scale it down to reach the extent exactly
+    var clamped_target_scale = Math.max(extent[0], Math.min(extent[1], target_scale));
+    if (clamped_target_scale != target_scale){
+        target_scale = clamped_target_scale;
+        factor = target_scale / scale;
+    }
 
-//     // Center each vector, stretch, then put back
-//     x = (x - center[0]) * factor + center[0];
-//     y = (y - center[1]) * factor + center[1];
+    // Center each vector, stretch, then put back
+    x = (x - center[0]) * factor + center[0];
+    y = (y - center[1]) * factor + center[1];
 
-//     // Transition to the new view over 100ms
-//     d3.transition().duration(100).tween("zoom", function () {
-//         var interpolate_scale = d3.interpolate(scale, target_scale),
-//             interpolate_trans = d3.interpolate(translate, [x,y]);
-//         return function (t) {
-//             zoom.scale(interpolate_scale(t))
-//                 .translate(interpolate_trans(t));
-//             zoomed();
-//         };
-//     }).each("end", function(){
-//         if (pressed) zoomButton(zoom_in);
-//     });
-// }
+    // Transition to the new view over 100ms
+    d3.transition().duration(100).tween("zoom", function () {
+        var interpolate_scale = d3.interpolate(scale, target_scale),
+            interpolate_trans = d3.interpolate(translate, [x,y]);
+        return function (t) {
+            zoom.scale(interpolate_scale(t))
+                .translate(interpolate_trans(t));
+            zoomed();
+        };
+    }).each("end", function(){
+        if (pressed) zoomButton(zoom_in);
+    });
+    zoomed();
+
+}
 
 function update(){
   link = link.data(links, function(d) { return d.id; }); //resetting the key is important because otherwise it maps the new data to the old data in order
