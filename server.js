2bba101c9371d0b6bf802676c63af8a4f12a1615
Alice Ma <ma.alice95@gmail.com>
2018-04-21 15:24:51 -0700

    removed require.js

--- server.js
+++ server.js
@@ -14,14 +14,3 @@ app.listen(port);
 app.get("/", function(req, res) {
   res.redirect("/index.html");
 });
-
-requirejs.config({
-    baseUrl: 'lib',
-    paths: {
-        app: '../app'
-    }
-});
-
-// Start loading the main app file. Put all of
-// your application logic in there.
-requirejs(['app/main']);
