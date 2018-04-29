930013a62896169d704262dfdd07ffe8e192ae1c
Nikhil Patel <n.patel@berkeley.edu>
2018-04-29 11:57:49 -0700

    Allow CORS request to server from frontend

--- api/src/app.js
+++ api/src/app.js
@@ -22,6 +22,24 @@ mongoose.connect(mongoURL)
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
+//////////// Setting Headers (CORS) ////////////
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Content-Type, Accept, Origin, Referer, Accept, User-Agent')
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    // Pass to next layer of middleware
+    next();
+});
+////////////////////////////////////////////////
+
+
+
 const sessionOptions = {
     resave: false,              // don't save session if unmodified
     saveUninitialized: false,   // don't create session until something stored
