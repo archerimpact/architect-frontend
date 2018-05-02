cb41fd599c0e39d5e53a00ee79f86eff9336c355
Alice Ma <alicema@berkeley.edu>
2018-05-01 23:30:56 -0700

    fixed the force in the hulls

--- public/js/helpers/matrix.js
+++ public/js/helpers/matrix.js
@@ -130,29 +130,29 @@ export function matrixToGraph(event=null) {
   this.reloadNeighbors();
 }
 
-export function matrixToGraph2(event=null) {
-  for (var i = 0; i < this.adjacencyMatrix.length; i++) {
-    let node = this.adjacencyMatrix[i][i]
-    if (node === TO_ADD) {
-      this.addNode(i, event);
-    } else if (node === TO_REMOVE) {
-      this.removeNode(i);
-    } else if (node === TO_HIDE) {
-      this.hideNode(i);
-    }
-    for (var j = 0; j < this.adjacencyMatrix.length; j++) {
-      if (i === j) { continue; }
-      let link = this.adjacencyMatrix[i][j];
-      if (link === TO_ADD) {
-        this.addLink(i, j);
-      } else if (link === TO_REMOVE) {
-        this.removeLink(i, j);
-      } else if (link === TO_HIDE) {
-        this.hideLink(i, j);
-      }
-    }
-  }
-}
+// export function matrixToGraph2(event=null) {
+//   for (var i = 0; i < this.adjacencyMatrix.length; i++) {
+//     let node = this.adjacencyMatrix[i][i]
+//     if (node === TO_ADD) {
+//       this.addNode(i, event);
+//     } else if (node === TO_REMOVE) {
+//       this.removeNode(i);
+//     } else if (node === TO_HIDE) {
+//       this.hideNode(i);
+//     }
+//     for (var j = 0; j < this.adjacencyMatrix.length; j++) {
+//       if (i === j) { continue; }
+//       let link = this.adjacencyMatrix[i][j];
+//       if (link === TO_ADD) {
+//         this.addLink(i, j);
+//       } else if (link === TO_REMOVE) {
+//         this.removeLink(i, j);
+//       } else if (link === TO_HIDE) {
+//         this.hideLink(i, j);
+//       }
+//     }
+//   }
+// }
 
 export function addNode(i, event=null) {
   this.createNode(ENTITY, event);
@@ -217,6 +217,7 @@ export function addGroup(i) {
     // If we have found a particular gorup member, we will transfer over its links to new group node, and add to 'group'
     if (i === group[a]) { continue; } // don't do anything if it's a node
       // i is index of new group id, j is index of group member, k is index of group member's connected link
+    this.globalNodes[group[a]].group = this.globalNodes[i].id;
     this.reattachLinks(i, group[a]);
     this.nodes.splice(this.nodes.indexOf(this.globalNodes[group[a]]), 1);
   }
@@ -231,6 +232,9 @@ export function ungroup(i) {
   for (var a = 0; a < group.length; a++) {
     this.adjacencyMatrix[group[a]][group[a]] = TO_UNHIDE;
     this.adjacencyMatrix[group[a]][i] = NONEXISTENT;
+    this.globalNodes[group[a]].centroidx = this.globalNodes[i].fixedX = this.globalNodes[i].x;
+    this.globalNodes[group[a]].centroidy = this.globalNodes[i].fixedY = this.globalNodes[i].y;
+
     for (var b = 0; b < this.adjacencyMatrix[i].length; b++) {
       if (a === b) { continue; }
       if (this.adjacencyMatrix[group[a]][b] === DISPLAYED) { this.adjacencyMatrix[group[a]][b] = TO_UNHIDE; }
