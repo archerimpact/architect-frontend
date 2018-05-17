68607bebd7fca8e310fbc9d4fcc74855f908f377
Nikhil Patel <n.patel@berkeley.edu>
2018-05-17 14:49:40 -0400

    Add Alice's 1 line of code, remove debug statement

--- frontend/dashboard-client/src/pages/Canvas/Graph/components/helpers/matrix.js
+++ frontend/dashboard-client/src/pages/Canvas/Graph/components/helpers/matrix.js
@@ -47,6 +47,7 @@ export function setMatrix(nodes, links, byIndex=false) {
 
 export function addToMatrix(centerid, nodes, links) {
   var numNodes = nodes.length;
+  this.reloadIdToIndex();
 
   for (var i = 0; i < numNodes; i++) {
     if (this.idToIndex[nodes[i].id] == null) {
