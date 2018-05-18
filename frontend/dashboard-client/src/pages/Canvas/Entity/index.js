7053043a97159b6f0e49434050e8d9fe71e7dc17
Nikhil Patel <n.patel@berkeley.edu>
2018-05-18 03:15:01 -0400

    Render more types of links in sidebar detailed view

--- frontend/dashboard-client/src/pages/Canvas/Entity/index.js
+++ frontend/dashboard-client/src/pages/Canvas/Entity/index.js
@@ -33,17 +33,19 @@ class Entity extends Component {
     }
     nodes.map(n => nodeMap[n.id] = n.name)
     const aliases = links.filter(link => link.type === 'AKA' && (node.id === link.source || node.id === link.target));
-    const documents = links.filter(link => link.type === 'HAS_ID_DOC' && node.id === link.source);
-    const maybe_sames = links.filter(link => link.type === 'POSSIBLY_SAME_AS' && node.id === link.source);
-    const definitely_sames = links.filter(link => link.type.startsWith('HAS_'));
+    const maybe_sames = links.filter(link => link.type === link.type.startsWith('POSSIBLY_SAME_') && node.id === link.source);
+    const docs = links.filter(link => link.type === 'HAS_ID_DOC' && (node.id === link.source || node.id === link.target));
+    const locs = links.filter(link => link.type === 'HAS_KNOWN_LOCATION' && (node.id === link.source || node.id === link.target));
+    const sancs = links.filter(link => link.type === 'SANCTIONED_ON' && (node.id === link.source || node.id === link.target));
 
-    const otherLinks = {
-      'SIGNIFICANT_PART_OF': 'Significant part of',
-      'PROVIDING_SUPPORT_TO': 'Providing support to',
-      'OWNED_BY': 'Owned by',
-      'ACTING_FOR': 'Acting for'
-    };
-    const others = links.filter(link => Object.keys(otherLinks).includes(link.type) && node.id === link.source);
+    const sig = links.filter(link => link.type === 'SIGNIFICANT_PART_OF' && (node.id === link.source || node.id === link.target));
+    const prov = links.filter(link => link.type === 'PROVIDING_SUPPORT_TO' && (node.id === link.source || node.id === link.target));
+    const owned = links.filter(link => link.type === 'OWNED_BY' && (node.id === link.source || node.id === link.target));
+    const act = links.filter(link => link.type === 'ACTING_FOR' && (node.id === link.source || node.id === link.target));
+
+    // const others = links.filter(link => Object.keys(otherLinks).includes(link.type) && node.id === link.source);
+
+
     return (
       <div className="full-width">
         <div className="entity-header-wrapper">
@@ -58,17 +60,32 @@ class Entity extends Component {
           <h5 className="">Attributes</h5>
           <EntityAttributes node={node} poo="hi"/>
 
-          {aliases.length ? <h5>Aliases</h5> : null}
+          {aliases.length ? <h5 className="subheader">Aliases</h5> : null}
           {aliases.map(a => <EntityCard data={a} id={a.target} shouldFetch graph={this.props.graph}/> )}
 
-          {documents.length ? <h5>Documents</h5> : null}
-          {documents.map(a => <EntityCard data={a} id={a.target} shouldFetch graph={this.props.graph}/> )}
+          {docs.length ? <h5 className="subheader">Documents</h5> : null}
+          {docs.map(a => <EntityCard data={a} id={a.target} shouldFetch graph={this.props.graph}/> )}
 
-          {maybe_sames.length ? <h5>Possibly Same As</h5> : null}
+          {locs.length ? <h5 className="subheader">Locations</h5> : null}
+          {locs.map(a => <EntityCard data={a} id={a.target} shouldFetch graph={this.props.graph}/> )}
+
+          {sancs.length ? <h5 className="subheader">Sanction Events</h5> : null}
+          {sancs.map(a => <EntityCard data={a} id={a.target} shouldFetch graph={this.props.graph}/> )}
+
+          {maybe_sames.length ? <h5 className="subheader">Possibly Same As</h5> : null}
           {maybe_sames.map(a => <EntityCard data={a} id={a.target} shouldFetch graph={this.props.graph}/> )}
 
-          {others.length ? <h5>Associations</h5> : null}
-          {others.map(a => <EntityCard data={a} id={a.target} shouldFetch graph={this.props.graph}/>)}
+          {sig.length ? <h5 className="subheader">Significant Part Of</h5> : null}
+          {sig.map(a => <EntityCard data={a} id={a.target} shouldFetch graph={this.props.graph}/>)}
+
+          {prov.length ? <h5 className="subheader">Providing Support To</h5> : null}
+          {prov.map(a => <EntityCard data={a} id={a.target} shouldFetch graph={this.props.graph}/>)}
+
+          {owned.length ? <h5 className="subheader">Owned By</h5> : null}
+          {owned.map(a => <EntityCard data={a} id={a.target} shouldFetch graph={this.props.graph}/>)}
+
+          {act.length ? <h5 className="subheader">Acting For</h5> : null}
+          {act.map(a => <EntityCard data={a} id={a.target} shouldFetch graph={this.props.graph}/>)}
         </div>
       </div>
     )
