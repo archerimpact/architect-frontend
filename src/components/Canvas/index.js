79c1c9d276ec2398cd17fd9d6208aa3b3b9901cd
Akshath Sivaprasad <akshath@berkeley.edu>
2018-06-07 01:33:25 -0700

    hotfix type coercion

--- src/components/Canvas/index.js
+++ src/components/Canvas/index.js
@@ -23,10 +23,10 @@ class Canvas extends Component {
         if (this.props.match.params && this.props.match.params.investigationId) {
             this.props.dispatch(fetchProject(this.props.match.params.investigationId));
         }
-        if (this.props.currentNode !== null) {
+        if (this.props.currentNode != null) {
             this.props.history.push(this.baseUrl + '/entity/' + encodeURIComponent(this.props.currentNode.id))
         }
-        if (this.props.match.params && this.props.match.params.sidebarState === 'search' && this.props.match.params.query !== null) {
+        if (this.props.match.params && this.props.match.params.sidebarState === 'search' && this.props.match.params.query != null) {
             this.props.dispatch(fetchSearchResults(this.props.match.params.query));
         } else if (this.props.match.params && this.props.match.params.sidebarState === 'entity') {
             this.props.dispatch(fetchEntity(decodeURIComponent(this.props.match.params.query)));
@@ -34,18 +34,18 @@ class Canvas extends Component {
     }
 
     componentWillReceiveProps(nextprops) {
-        if (nextprops.currentNode !== null && this.props.currentNode !== nextprops.currentNode) {
+        if (nextprops.currentNode != null && this.props.currentNode != nextprops.currentNode) {
             this.props.history.push(this.baseUrl + '/entity/' + encodeURIComponent(nextprops.currentNode.id))
         }
         if (this.props.location.pathname !== nextprops.location.pathname && nextprops.match.params) {
             // this.props.actions.fetchProject(nextprops.match.params.investigationId);
             let nextQuery = nextprops.match.params.query;
             if (nextprops.match.params.sidebarState === 'search') {
-                if (nextQuery !== null && this.props.match.params.query !== nextQuery) {
+                if (nextQuery != null && this.props.match.params.query != nextQuery) {
                     this.props.dispatch(fetchSearchResults(nextQuery));
                 }
             } else if (nextprops.match.params.sidebarState === 'entity') {
-                if (nextQuery !== null && this.props.match.params.query !== nextQuery) {
+                if (nextQuery != null && this.props.match.params.query != nextQuery) {
                     this.props.dispatch(fetchEntity(decodeURIComponent(nextprops.match.params.query)));
                 }
             }
