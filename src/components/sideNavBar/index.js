f08f325a7d0d028c7d266629b766c9c9f56059c1
Akshath Sivaprasad <akshath@berkeley.edu>
2018-06-08 11:15:03 -0700

    navbar tooltips work

--- src/components/sideNavBar/index.js
+++ src/components/sideNavBar/index.js
@@ -1,41 +1,43 @@
 import React from 'react';
 import { withRouter, Link } from 'react-router-dom';
-import { connect } from "react-redux";
-import { resetProjectDispatch } from "../../redux/actions/graphActions";
+import { connect } from 'react-redux';
+import ReactTooltip from 'react-tooltip';
+import { resetProjectDispatch } from '../../redux/actions/graphActions';
 
 import './style.css'
 
 const SideNavBar = (props) => {
   return (
     <div className="side-nav">
+      <ReactTooltip place="right" effect="solid"/>
       <Link to='/' onClick={() => { props.dispatch(resetProjectDispatch()); }}>
-        <div className="side-nav-button">
+        <div className="side-nav-button" data-tip="Home">
           <i className="material-icons">home</i>
         </div>
       </Link>
       <Link to='/data'>
-        <div className="side-nav-button">
+        <div className="side-nav-button" data-tip="Data">
           <i className="material-icons">data_usage</i>
         </div>
       </Link>
       <Link to='/build' onClick={() => { props.dispatch(resetProjectDispatch()); }}>
-        <div className="side-nav-button">
+        <div className="side-nav-button" data-tip="Spaces">
           <i className="material-icons">dashboard</i>
         </div>
       </Link>
       <Link to='/user'>
-        <div className="side-nav-button">
+        <div className="side-nav-button" data-tip="Account">
           <i className="material-icons">account_box</i>
         </div>
       </Link>
       <div className="bottom">
         <Link to='/settings'>
-          <div className="side-nav-button">
+          <div className="side-nav-button" data-tip="Settings">
             <i className="material-icons">settings</i>
           </div>
         </Link>
         <Link to='/help'>
-          <div className="side-nav-button">
+          <div className="side-nav-button" data-tip="Help">
             <i className="material-icons">help</i>
           </div>
         </Link>
