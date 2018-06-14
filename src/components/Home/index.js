c61ae1fd1b5b6e92b73c9fd7071139a38358ac61
Akshath Sivaprasad <akshath@berkeley.edu>
2018-06-14 15:25:20 -0700

    basic aesthetic done

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,28 @@
 import React, {Component} from "react";
 import DatabaseSearchBar from "../databaseSearchBar";
+import Canvas from "../Canvas";
 
 import "./style.css";
+import logo from "../../images/architect-logo-light-transparent-cropped.png";
+import decal from "../../images/decal-up-gradient-blue.png";
+import decalFlipped from "../../images/decal-up-gradient-blue-flipped.png";
 
 export default class Home extends Component {
-
     render() {
         return (
-            <div className='home-container' style={{height: '100%'}}>
-                <h1 className="home-header">ARCHITECT</h1>
-                <p className="home-subheader">A World of Data at Your Fingertips</p>
-                <div className="search-main">
-                    <DatabaseSearchBar showSettings={false}/>
+            <div className="home">
+                <div className="home-cover">
+                    <div className="center">
+                        <img className="logo" src={logo} alt="logo"></img>
+                        <DatabaseSearchBar homeSearchContainerId="home-search-container" homeSearchInputId="home-search-input" showSettings={false}/>
+                    </div>
+                    <div className="decal-container">
+                        <img className="decal" src={decal} alt="decal"></img>
+                        <img id="right-decal" className="decal" src={decalFlipped} alt="decal"></img>
+                        <img id="right-decal" className="decal" src={decal} alt="decal"></img>
+                    </div>
                 </div>
+                <Canvas />
             </div>
         );
     }
