a76d45b45e804b5c4a17dcd6d6da07f1b8fe5cd8
Ali Ahmed <ali.ahmed@berkeley.edu>
2018-07-04 16:00:38 +0100

    sidebar opens and closes onClick of icon for more intuitive nav. Also removed settings

--- src/components/Canvas/graphSidebar/index.js
+++ src/components/Canvas/graphSidebar/index.js
@@ -8,7 +8,6 @@ import {connect} from "react-redux";
 import "./style.css";
 import * as actions from "../../../redux/actions/projectActions";
 
-
 class GraphSidebar extends Component {
     constructor(props) {
         super(props);
@@ -20,10 +19,10 @@ class GraphSidebar extends Component {
         };
     }
 
-    componentDidMount() {
+    componentWillMount() {
         let listener = this.props.history.listen((location, action) => {
             this.setState({history: [...this.state.history, location]});
-        })
+        });
         this.setState({listener: listener})
     }
 
@@ -40,50 +39,49 @@ class GraphSidebar extends Component {
         }
     }
 
+
+
+    toggleSidebar = (tabName) => {
+        const doNothing = () => {};
+        const toggleSidebar = () => this.props.dispatch(actions.toggleSidebar());
+        if (tabName === "toggleSidebar") {
+            toggleSidebar();
+        } else {
+            !this.props.sidebarVisible ? toggleSidebar() : doNothing()
+        }
+    };
+
     renderTabs = () => {
         let baseUrl = '/build/' + this.props.match.params.investigationId;
         const activeState = this.props.match.params.sidebarState;
 
         return (
             <div className="tabs" key="tabs">
-                <div className={"tab " + (activeState === 'search' ? 'active-tab' : '')}>
+                <div className={"tab " + (activeState === 'search' ? 'active-tab' : '')} onClick={() => this.toggleSidebar("search")}>
                     <Link to={baseUrl + '/search'}>
                         <div>
                             <i className="tab-icon material-icons">search</i>
                         </div>
                     </Link>
                 </div>
-                <div className={"tab " + (activeState === 'entity' ? 'active-tab' : '')}>
+                <div className={"tab " + (activeState === 'entity' ? 'active-tab' : '')} onClick={() => this.toggleSidebar("entity")}>
                     <Link to={baseUrl + '/entity'}>
                         <div>
                             <i className="tab-icon material-icons">description</i>
                         </div>
                     </Link>
                 </div>
-                <div className={"tab " + (activeState === 'list' ? 'active-tab' : '')}>
+                <div className={"tab " + (activeState === 'list' ? 'active-tab' : '')} onClick={() => this.toggleSidebar("list")}>
                     <Link to={baseUrl + '/list'}>
                         <div>
                             <i className="tab-icon material-icons">list</i>
                         </div>
                     </Link>
                 </div>
-                <div className={"tab " + (activeState === 'settings' ? 'active-tab' : '')}>
-                    <Link to={baseUrl + '/settings'}>
-                        <div>
-                            <i className="tab-icon material-icons">settings</i>
-                        </div>
-                    </Link>
-                </div>
-                <div className="tab" onClick={() => this.props.dispatch(actions.toggleSidebar())}>
+                <div className="tab" onClick={() => this.toggleSidebar("toggleSidebar")}>
                     <i className="tab-icon material-icons">{this.props.sidebarVisible ? "chevron_right" : "chevron_left"}</i>
                 </div>
             </div>
-        )
-    }
-
-    renderSettings = () => {
-        return (
-            <div> Sample text </div>
         )
     }
 
@@ -104,12 +102,10 @@ class GraphSidebar extends Component {
                 return <Entity graph={this.props.graph} id={this.props.match.params.query}/>
             case "list":
                 return <ProjectData graph={this.props.graph}/>
-            case "settings":
-                return this.renderSettings();
             default:
                 return null
         }
-    }
+    };
 
     render() {
         return (
