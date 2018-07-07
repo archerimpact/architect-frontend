d2e10e1ecc4503a325950c9e75d9749a057ad411
Nikhil Patel <n.patel@berkeley.edu>
2018-07-07 11:50:59 +0100

    Minor styling adjustments to vignette date/author

--- src/components/Home/Vignette/index.js
+++ src/components/Home/Vignette/index.js
@@ -18,8 +18,9 @@ class Vignette extends Component {
             <div className="vignette-card-row flex-row">
               <div className="vignette-card-col vignette-card-left-col">
                 <div className="vignette-card-header">
+                <p className="vignette-date">July 9, 2018</p>
                   <h4 className="vignette-title">How to Evade OFAC Sanctions</h4>
-                  <p className="vignette-author">Archer Team - JULY 6, 2018</p>
+                  <p className="vignette-author">by Archer Team</p>  
                   <hr className="vignette-divider" />
                 </div>
 
@@ -38,14 +39,13 @@ class Vignette extends Component {
 
               <div className="vignette-card-col vignette-card-right-col">
                 <div className="vignette-card-graph">
-                  <p>I am a graph</p>
+
                 </div>
                 <div className="vignette-card-footer flex-row">
                   <div className="vignette-share-icons">
                     <span className="vignette-twitter-action material-icons">share</span>
                     <span className="vignette-link-action material-icons">link</span>
                   </div>
-
                   <button className="btn btn-primary vignette-explore-button ml-auto">Explore In Depth</button>
                 </div>
               </div>
