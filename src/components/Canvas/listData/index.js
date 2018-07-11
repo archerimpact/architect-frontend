4e967f7bc05b4271c6b3bf6ad6e7bcd97b10dc84
Ali Ahmed <ali.ahmed@berkeley.edu>
2018-07-11 17:15:11 +0100

    fixed list search

--- src/components/Canvas/listData/index.js
+++ src/components/Canvas/listData/index.js
@@ -5,7 +5,7 @@ import {bindActionCreators} from "redux";
 import {withRouter} from "react-router-dom";
 import * as graphActions from "../../../redux/actions/graphActions";
 import EntityCard from "../entityCard";
-
+import * as Fuse from "fuse.js"
 import "./style.css";
 
 class ListData extends Component {
@@ -16,61 +16,13 @@ class ListData extends Component {
         }
     }
 
-    getLevenshteinDistance = (node, query) => {
-        let sumDistances = 0;
-        if (node.hasOwnProperty("name")) {
-            for (let i=0; i<node.name.length; i++) {
-                let tempDistance = levenshtein.get(node.name.slice(0, i).toLowerCase(), query.toLowerCase())
-                if (tempDistance === 0) {
-                    return tempDistance
-                }
-                sumDistances += tempDistance
-            }
-            return sumDistances / node.name.length
-        } else if (node.hasOwnProperty("combined")) {
-            for (let i=0; i<node.combined.length; i++) {
-                let tempDistance = levenshtein.get(node.combined.slice(0, i).toLowerCase(), query.toLowerCase())
-                if (tempDistance === 0) {
-                    return tempDistance
-                }
-                sumDistances += tempDistance
-            }
-            return sumDistances / node.combined.length
-        } else if (node.hasOwnProperty("number")) {
-            for (let i=0; i<node.number.length; i++) {
-                let tempDistance = levenshtein.get(node.number.slice(0, i).toLowerCase(), query.toLowerCase())
-                if (tempDistance === 0) {
-                    return tempDistance
-                }
-                sumDistances += tempDistance
-            }
-            return sumDistances / node.number.length
-        } else if (node.hasOwnProperty("description")) {
-            for (let i=0; i<node.description.length; i++) {
-                let tempDistance = levenshtein.get(node.description.slice(0, i).toLowerCase(), query.toLowerCase())
-                if (tempDistance === 0) {
-                    return tempDistance
-                }
-                sumDistances += tempDistance
-            }
-            return sumDistances / node.description.length
-        }
-    };
-
     fetchListResults = (query) => {
         let nodes = this.props.data.nodes;
-        let searchResults = [];
-        searchResults.push({ node: nodes[0], distance: this.getLevenshteinDistance(nodes[0], query) });
-        for (let i=1; i<nodes.length; i++) {
-            let distance = this.getLevenshteinDistance(nodes[i], query)
-            for (let j=0; j<searchResults.length; j++) {
-                if (distance <= searchResults[j].distance) {
-                    searchResults.splice(j, 0, { node: nodes[i], distance })
-                    break;
-                }
-            }
-        }
-        return searchResults
+        let options = {
+            keys: ['name', 'combined', 'label', 'description']
+        };
+        let fuse = new Fuse(nodes, options);
+        return  fuse.search(query);
     };
 
     componentWillReceiveProps(nextprops) {
@@ -93,7 +45,7 @@ class ListData extends Component {
                 <div className="search-results">
                     {
                         this.state.listDataSearchResults.length !== 0 ?
-                        this.state.listDataSearchResults.map(nodeDict => <EntityCard key={nodeDict.node.id} node={nodeDict.node} graph={graph} data={data}/>)
+                        this.state.listDataSearchResults.map(node => <EntityCard key={node.id} node={node} graph={graph} data={data}/>)
                             :
                         this.props.data.nodes.map(node => <EntityCard key={node.id} node={node} graph={graph} data={data}/>)
                     }
