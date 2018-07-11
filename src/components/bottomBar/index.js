54a9f892c05d6de79af68e630f2ada7b4ffa6b9d
Nikhil Patel <n.patel@berkeley.edu>
2018-07-11 22:07:07 +0100

    Add mailchimp to bottom bar

--- src/components/bottomBar/index.js
+++ src/components/bottomBar/index.js
@@ -29,8 +29,19 @@ export default class BottomBar extends Component {
               <p className="bottombar-text">
                 We're just getting started &mdash; sign up to access the full platform!
               </p>
-              <input className="bottombar-input form-control" type="text" placeholder="Email address" />
-              <i className="bottombar-submit material-icons">send</i>
+
+              
+
+              <form className="modal-form" action="https://archeratberkeley.us17.list-manage.com/subscribe/post?u=6f7abb9e526fd4f56b65d0305&amp;id=8399a2fbfc" method="post" id="subForm" data-error="Please fill all fields correctly." data-success="Thanks for signing up! Please check your inbox for confirmation email.">
+                <div className="flex-row bottom-email-row">
+                  <input id="bottom-email-input" className="bottombar-input form-control" type="text" id="mce-EMAIL" name="EMAIL" type="email" placeholder="Email Address" required />
+                  <button id="bottom-email-submit" className="btn btn-primary" type="submit" >
+                    <i className="bottombar-submit material-icons">send</i>
+                  </button>
+                </div>
+              </form>
+
+
             </div>
           )
           :
