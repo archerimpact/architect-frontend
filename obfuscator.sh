2d8252469a58742d3c4089f0652736705c37d01b
Akshath Sivaprasad <akshath@berkeley.edu>
2018-07-12 11:44:46 -0700

    added self defending flag

--- obfuscator.sh
+++ obfuscator.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo rm -rf build
 sudo npm run build
-sudo javascript-obfuscator ./build/static/js/ --output . --dead-code-injection true --dead-code-injection-threshold 0.1
+sudo javascript-obfuscator ./build/static/js/ --output .  --compact true --dead-code-injection true --dead-code-injection-threshold 0.1  --self-defending true
