aefcedd3457cdc52eacfe8478bc13f3e0babcbbb
Akshath Sivaprasad <akshath@berkeley.edu>
2018-07-12 11:28:00 -0700

    remove string array flag

--- obfuscator.sh
+++ obfuscator.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo rm -rf build
 sudo npm run build
-sudo javascript-obfuscator ./build/static/js/ --output . --control-flow-flattening true --dead-code-injection true --dead-code-injection-threshold 0.1 --transform-object-keys true  --disable-console-output true --string-array true 
+sudo javascript-obfuscator ./build/static/js/ --output . --control-flow-flattening true --dead-code-injection true --dead-code-injection-threshold 0.1 --transform-object-keys true
