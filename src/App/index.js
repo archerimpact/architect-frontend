509d8c7cebf1f45c871f2990336d5146fef04962
Ali Ahmed <ali.ahmed@berkeley.edu>
2018-07-18 10:34:06 +0100

    porting over auth files and routes

--- src/App/index.js
+++ src/App/index.js
@@ -1,15 +1,26 @@
 import React, {Component} from "react";
 import {Route} from "react-router-dom";
+import Login from "../components/Login";
 import Canvas from "../components/Canvas";
 
 import "./style.css";
 
 export default class App extends Component {
+
+    logOut() {
+        return this.props.dispatch(userLogOut());
+    }
+
+    logIn() {
+        return (<Redirect to={'/login'}/>);
+    }
+
     render() {
         return (
           <div>
             <div className="main">
-                <Route path="/explore/:sidebarState?/:query?" component={Canvas}/>
+                <Route path="/login" component={Login}/>
+                <PrivateRoute path="/explore/:sidebarState?/:query?" component={Canvas}/>
             </div>
           </div>
         );
