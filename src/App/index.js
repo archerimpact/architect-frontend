451a573178c4d6e3a11d7cf566c2f574d8b18721
Ali Ahmed <ali.ahmed@berkeley.edu>
2018-07-18 12:18:09 +0100

    fixed message for IE and phone

--- src/App/index.js
+++ src/App/index.js
@@ -7,14 +7,40 @@ import SideNavbar from '../components/sideNavBar';
 import "./style.css";
 
 export default class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            IE: false,
+            phone: false
+        }
+    }
+
+    componentWillMount() {
+        if (navigator.appName === 'Microsoft Internet Explorer' || 'ActiveXObject' in window) {
+            this.setState({IE: true})
+        }
+
+        if (window.innerWidth < 678) {
+            this.setState({phone: true})
+        }
+    }
+
     render() {
-        return (
-          <div>
-            <div className="main">
-                <Route exact path="/:id?" component={Home}/>
-                <Route path="/explore/:sidebarState?/:query?" component={Canvas}/>
-            </div>
-          </div>
-        );
+        const { IE, phone } = this.state;
+        if (IE) {
+            return <h1 className="special-message" style={{width: window.innerWidth}}>Unfortunately, Internet Explorer is not yet a supported browser, though we are working on supporting it.  Please access the site using a browser such as Chrome or Firefox.</h1>
+        } else if (phone) {
+            return <h1 className="special-message" style={{width: window.innerWidth}}>Please use a laptop for a full interactive experience.</h1>
+        }
+        else {
+            return (
+                <div>
+                    <div className="main">
+                        <Route exact path="/:id?" component={Home}/>
+                        <Route path="/explore/:sidebarState?/:query?" component={Canvas}/>
+                    </div>
+                </div>
+            );
+        }
     }
 }
